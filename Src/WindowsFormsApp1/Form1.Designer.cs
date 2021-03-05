5b16c479f9ba0d487bcc11400c4c29d0db9bfb25
metz97 <41136570+metz97@users.noreply.github.com>
2021-03-05 11:36:09 +0700

    Initial Commit

---
+++ Src/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKataKunci = new System.Windows.Forms.TextBox();
+            this.txtParagraf = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(195, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kata Kunci";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(195, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Paragraf";
+            // 
+            // txtKataKunci
+            // 
+            this.txtKataKunci.Location = new System.Drawing.Point(308, 76);
+            this.txtKataKunci.Name = "txtKataKunci";
+            this.txtKataKunci.Size = new System.Drawing.Size(188, 20);
+            this.txtKataKunci.TabIndex = 2;
+            this.txtKataKunci.TextChanged += new System.EventHandler(this.txtKataKunci_TextChanged);
+            // 
+            // txtParagraf
+            // 
+            this.txtParagraf.Location = new System.Drawing.Point(308, 134);
+            this.txtParagraf.Name = "txtParagraf";
+            this.txtParagraf.Size = new System.Drawing.Size(188, 20);
+            this.txtParagraf.TabIndex = 3;
+            this.txtParagraf.TextChanged += new System.EventHandler(this.txtParagraf_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(198, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Total Kata Kunci";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(305, 193);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(13, 13);
+            this.lblTotal.TabIndex = 5;
+            this.lblTotal.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtParagraf);
+            this.Controls.Add(this.txtKataKunci);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKataKunci;
+        private System.Windows.Forms.TextBox txtParagraf;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ Src/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtKataKunci_TextChanged(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtKataKunci.Text) && !String.IsNullOrEmpty(txtParagraf.Text))
+                lblTotal.Text = (txtParagraf.Text.Split(new string[] { txtKataKunci.Text },
+                    StringSplitOptions.None).Length - 1).ToString();
+            else
+                lblTotal.Text = "0";
+        }
+
+        private void txtParagraf_TextChanged(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(txtKataKunci.Text) && !String.IsNullOrEmpty(txtParagraf.Text))
+                lblTotal.Text = (txtParagraf.Text.Split(new string[] { txtKataKunci.Text },
+                    StringSplitOptions.None).Length - 1).ToString();
+            else
+                lblTotal.Text = "0";
+        }
+    }
+}
